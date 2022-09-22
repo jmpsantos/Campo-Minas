2d191d62bf7d018a3a273b612bccffb92c8bcc4c
João Santos <jmpsantos148@gmail.com>
2022-09-22 16:00:50 +0100

    Add files via upload

---
+++ G06.Minesweeper.Common/Diversos/Delegados.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G06.Minesweeper.Common.Diversos
+{
+        public delegate void DelegadoVazio();
+        public delegate void DelegadoINT(int valor);
+        public delegate void DelegadoSTRING(string str);
+        public delegate void DelegadoRegisto(string nome, string username, string email, string password, string pais, string imagem);
+        public delegate void DelegadoLogin(string email, string password);
+        public delegate void DelegadoVencedor(string nome, int email);
+}
---
+++ G06.Minesweeper.Common/Diversos/Interfaces.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G06.Minesweeper.Common.Diversos
+{
+    public interface JanelaPrincipal
+    {
+        event DelegadoVazio UserQuerAjuda;
+        event DelegadoINT UserQuerNovoJogo;
+        event DelegadoVazio UserQuerJogarOnline;
+        event DelegadoINT UserQuerConsultarMelhoresTempos;
+        event DelegadoVazio UserQuerSair;
+        event DelegadoVazio UserQuerConsultarTop10;
+        event DelegadoVazio EstadoLoginAlterado;
+    }
+
+    public interface JanelaAjuda
+    {
+        event DelegadoVazio UserQuerSair;
+        //event DelegadoVazio UserQuerConsultarYT;
+        //event DelegadoVazio UserQuerConsultarWikipedia;
+    }
+
+    public interface JanelaConsultarMelhoresTempos
+    {
+        event DelegadoVazio UserQuerSair;
+    }
+
+    public interface JanelaConsultarTop10
+    {
+        event DelegadoVazio UserQuerSair;
+    }
+
+    public interface JanelaRegistar
+    {
+        event DelegadoRegisto UserQuerRegistar;
+        event DelegadoVazio UserQuerSair;
+    }
+
+    public interface JanelaDadosVencedor
+    {
+        event DelegadoVencedor UserQuerRegistarNome;
+        event DelegadoVazio UserNabo;
+    }
+
+    public interface JanelaLoadingOnline
+    {
+        event DelegadoVazio IniciarJanelaLogin;
+    }
+
+    public interface JanelaLogin
+    {
+        event DelegadoLogin UserQuerFazerLogin;
+        event DelegadoVazio UserQuerSair;
+        event DelegadoVazio UserQuerFazerRegisto;
+    }
+}
